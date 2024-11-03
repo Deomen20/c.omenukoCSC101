ce99edd3251553fdd55eea94efafdae8b356ad4a
Deomen20 <derahomenz2@gmail.com>
2024-11-03 23:22:17 +0100

    (My Third commit)

--- week-1/practice_1.rs
+++ week-1/practice_1.rs
@@ -1,3 +1,3 @@
-fn fn main() {
-	printin!("Welcome to CSC 101!");
-	
+fn main(){
+	println!("Welcome to CSC 101");
+}
---
+++ week-3/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\Chidera\ Omenuko\Documents\c.omenukoCSC101\week-3\practice_7\target\debug\practice_7.exe: C:\Users\Chidera\ Omenuko\Documents\c.omenukoCSC101\week-3\practice_7\src\main.rs
